9066aa2141e68f259d5b3693a7363406f341ea5b
DianaBayazitova <71555851+DianaBayazitova@users.noreply.github.com>
2020-10-12 14:50:40 +0300

    Add files via upload

---
+++ Parametr.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Lab1
+{
+    public static class Parametr
+    {
+
+        public static int GetIntValue(string value)
+        {
+            int res = -1;
+            try
+            {
+                res = int.Parse(value);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Parametr isn't int");
+            }
+            return res;
+        }
+
+
+        public static double GetDoubleValue(string value)
+        {
+            double res = -1;
+            try
+            {
+                res = double.Parse(value);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Parametr isn't double");
+            }
+            return res;
+        }
+
+        public static string GetStringValue(string value)
+        {
+            return value;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+
+namespace Lab1
+{
+    class Program
+    {
+        static void Main()
+        {
+            IniFile file = new IniFile();
+            file.Load("test.ini");
+            file.Print();
+            //Console.WriteLine(file.GetInt("[COMMON]", "StatisterTimeMs"));
+            //Console.WriteLine(file.GetDouble("[ADC_DEV]", "BufferLenSeconds"));
+            Console.WriteLine(file.GetString("[ADC_DEV]", "Driver"));
+        }
+    }
+}
